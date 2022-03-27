9993106352beb2d53ae60fcf7e355d26f431ffd0
Amber Bauer <amberbauer@Ambers-MacBook-Pro.local>
2022-03-27 14:10:21 -0400

    added licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 //Included packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
 
 // Created an array of questions for user input
 const questions = [
@@ -11,6 +12,32 @@ const questions = [
         name: 'project title',
         message: 'What is your project title?'
       },
+//2. Project description
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project.'
+},
+
+//3. Installation
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+},
+//4. Useage
+{
+    type: 'input',
+    name: 'useage',
+    message: 'Provide instructions and examples for use. Include screenshots as needed.'
+},
+//5. License
+{
+    type: 'rawlist',
+    name: 'license',
+    message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    choices: ['No License, Apache license 2.0, GNU Affero General Public License v3.0,MIT License, BSD 2-clause "Simplified" license, BSD 3-clause "New" or "Revised" license,Boost Software License 1.0, Creative Commons Zero v1.0 Universal, Eclipse Public License 2.0, GNU Affero General Public License v3.0,GNU General Public License v2.0,GNU Lesser General Public License v2.1, Mozilla Public License 2.0,The Unlicense,']
+},
 
 ];
 
